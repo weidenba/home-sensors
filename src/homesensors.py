6453aae00c6205a4e1a2ac8b5c3e28ed85ed5363
Peter Weidenbach <weidenba@gmail.com>
2020-10-15 19:49:42 +0200

    template added

--- src/homesensors.py
+++ src/homesensors.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from sensor_io.dht22 import get_current_sensor_value
-from filter.convert import unix_to_hr_time
+from filters.convert import unix_to_hr_time
 
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     sensor_value = get_current_sensor_value()
-    return "{} Temp={0.1f}*C  Humidity={0.1f}%".format(unix_to_hr_time(sensor_value.timestamp), sensor_value.temperature, sensor_value.humidity)
+    return render_template('current.html', timestamp=unix_to_hr_time(sensor_value.timestamp), humdity=sensor_value.humidity, temperature=sensor_value.temperature)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
